6bec854f278f9f957bb7d3b05055ca50014b44b9
jgjorge96 <jgjorge96@gmail.com>
2022-04-20 12:18:18 -0300

    listo turnos

---
+++ turnos.js
@@ -0,0 +1,24 @@
+function gotowhatsapp() {
+    
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var telefono = document.getElementById("telefono").value;
+    var tratamiento = document.getElementById("tratamiento").value;
+    var profesional = document.getElementById("profesional").value;
+    var fecha = document.getElementById("fecha").value;
+    var horario = document.getElementById("horario").value;
+    var comentarios = document.getElementById("comentarios").value;
+
+    var url = "https://wa.me/+5493515154737?text=" 
+    + "Hola! Quiero solicitar un turno" + "%0a"
+    + "%0a" + "Nombre del Paciente: " + nombre + "%0a"
+    + "Email: " + email  + "%0a"
+    + "Telefono: " + telefono + "%0a"
+    + "Tratamiento: " + tratamiento + "%0a"
+    + "Profesional: " + profesional + "%0a"
+    + "Fecha: " + fecha + "%0a"
+    + "Horario: "+horario + "%0a"
+    + "%0a" + comentarios; 
+
+    window.open(url, '_blank').focus();
+}
